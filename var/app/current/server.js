512cc33a3ee2486c824fa15e076d355d52e024c3
root <root@ip-10-0-1-192.ap-northeast-2.compute.internal>
2023-11-05 21:05:37 +0000

    Add server.js and package.json for deployment2

--- var/app/current/server.js
+++ var/app/current/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello World5397');
+  res.send('Hello World57');
 });
 
 const PORT = process.env.PORT || 3000;
